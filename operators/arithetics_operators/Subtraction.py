bcd304beb754484a365589b1b8daef9bbee1133b
Jose Andre <joandre@c3r6s5.42luanda.com>
2026-05-12 11:41:56 +0100

    multiplication numbers

---
+++ operators/arithetics_operators/Subtraction.py
@@ -0,0 +1,6 @@
+#Subtraction of the several numbers
+
+n1 = 20
+n2  = 5
+
+print(f"the subt of those numbers it is: {n1 - n2}")
---
+++ operators/arithetics_operators/multiplication.py
@@ -0,0 +1,5 @@
+#Multiplication
+
+n1 = 2
+n2 = 5
+print(f"Operation: {n1} * {n2} = {n1 * n2}")
