438171bdb2ee01e460ca29875cbf06850899aafd
Jose Pedro Fuxi Andre <josefuxi2000@gmail.com>
2026-08-26 19:48:09 +0100

    add ft_garden function to introduce the garden program

---
+++ module_01(Object-Oriented Garden Systems)/ex01(Plantig your first seed)/ft_garden_intro.py
@@ -0,0 +1,9 @@
+#! /bin/python3
+
+def ft_garden():
+    print("== Welcome to My Garden ==")
+    print("Plant: Rose\nHeigth: 25cm\nAge: 30 days\n")
+    print("== End of Program ==")
+
+if __name__ == "__main__" :
+    ft_garden()
