8a2d9a290a065bb522a7e800bd2a0772e2c8279f
Jose Pedro Fuxi Andre <josefuxi2000@gmail.com>
2026-08-26 18:18:01 +0100

    fix ft_seed_inventory function to correctly handle unit types

--- module_00/ex07/ft_seed_inventory.py
+++ module_00/ex07/ft_seed_inventory.py
@@ -1,10 +1,12 @@
 def  ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     seed_type = seed_type[0].upper() + seed_type[1:]
-    if unit.find("packet") == 0:
+    if unit.find("packets") == 0:
         print(f'{seed_type} seeds: {quantity} {unit} available')
-    elif unit.find("gram") == 0:
+    elif unit.find("grams") == 0:
         print(f'{seed_type} seeds: {quantity} {unit} total')
     elif unit.find("area") == 0:
         print(f'{seed_type} seed: covers {quantity} square metters')
+    else:
+        print("Unknown unit type")
         
 ft_seed_inventory("tomato", 12, "packet")
