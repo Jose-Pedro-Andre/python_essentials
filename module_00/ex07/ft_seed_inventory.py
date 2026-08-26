3e910e588799e54bef37147659bcdda634168d9a
Jose Pedro Fuxi Andre <josefuxi2000@gmail.com>
2026-08-26 18:13:38 +0100

    add ft_seed_inventory function for managing seed inventory

---
+++ module_00/ex07/ft_seed_inventory.py
@@ -0,0 +1,10 @@
+def  ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    seed_type = seed_type[0].upper() + seed_type[1:]
+    if unit.find("packet") == 0:
+        print(f'{seed_type} seeds: {quantity} {unit} available')
+    elif unit.find("gram") == 0:
+        print(f'{seed_type} seeds: {quantity} {unit} total')
+    elif unit.find("area") == 0:
+        print(f'{seed_type} seed: covers {quantity} square metters')
+        
+ft_seed_inventory("tomato", 12, "packet")
